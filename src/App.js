264e80a860ce8dfc0ab31cebfdcf061101d2c7e7
Elise Pitre <pitre@uoguelph.ca>
2026-01-07 20:03:52 -0500

    Changing Fonts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";  // Import Header
 import "./styles/App.css";
+import "@fontsource/space-grotesk"; // Import Space Grotesk font
+import "@fontsource/hachi-maru-pop"; // Import hachi maru pop font
+
 
 function App() {
   return (
